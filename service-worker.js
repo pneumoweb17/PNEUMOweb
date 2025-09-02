e212925d9ca37aa38f930bfb365a7a6a9323a2de
pneumoweb <tipanichilsi@gmail.com>
2025-09-02 21:19:28 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,25 @@
-const CACHE_NAME = "myproject-cache-v1";
+const CACHE_NAME = "PNEUMOweb-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/pages.html",
-  "/result.html",
-  "/screening.html",
-  "/style.css",
-  "/pages.css",
-  "/result.css",
-  "/screening.css",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/logo.png",
-  "/background.png",
-  "/background2.png"
+  "index.html",
+  "pages.html",
+  "result.html",
+  "screening.html",
+  "style.css",
+  "pages.css",
+  "result.css",
+  "screening.css",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "logo.png",
+  "background.png",
+  "background2.png"
 ];
 
 // Install and cache files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
